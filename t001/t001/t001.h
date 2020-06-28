169679c47c01565026799d97265cdecbf9db1f5f
3179901003 <3179901003@zju.edu.cn>
2020-06-28 10:40:52 +0800

    test02

---
+++ t001/t001/t001.h
@@ -0,0 +1,32 @@
+﻿
+// t001.h: PROJECT_NAME 应用程序的主头文件
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "在包含此文件之前包含 'pch.h' 以生成 PCH"
+#endif
+
+#include "resource.h"		// 主符号
+
+
+// Ct001App:
+// 有关此类的实现，请参阅 t001.cpp
+//
+
+class Ct001App : public CWinApp
+{
+public:
+	Ct001App();
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+
+// 实现
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern Ct001App theApp;
